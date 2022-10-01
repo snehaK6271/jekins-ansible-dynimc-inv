894a8dccf6c97e21b8cde7092ab35abf2b782a9f
Sneha Kotturthi <snehakedaresh627@gmail.com>
2022-10-01 18:30:40 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "AWSEC2Instance"{
      count   = 2
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
-     security_groups = ["default"]
+     security_groups = ["launch-wizard-27"]
      key_name        = "sneha"
      tags = {
         Name = "tomcatservers"
